1993a6035380a4d31591d337c45f264f09b29e2c
Divyanshu Madan <iamdm99@gmail.com>
2020-07-15 02:12:29 +0530

    [gh.rs] multiple fixes
    - specific import
    - derive default
    - use Option instead of doing default values later
    - Array3 to Array1

--- filters/src/gh.rs
+++ filters/src/gh.rs
@@ -1,11 +1,13 @@
-use ndarray::prelude::*;
+use ndarray::Array1;
 
-#[allow(dead_code)]
+#[derive(Default)]
 struct GhFilterOrder<T> {
-  order: u32,
-  g: T,
-  h: T,
-  k: T,
-  x: Array3::<T>,
-  dt: T,
+    order: u32,
+    g: T,
+    h: Option<T>,
+    k: Option<T>,
+    x: Array1<T>,
+    y: Array1<T>,
+    z: Array1<T>,
+    dt: T,
 }
