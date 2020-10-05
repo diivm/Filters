e72103d385ae49d628c6a6e19577faf0f54400be
Divyanshu Madan <iamdm99@gmail.com>
2020-10-06 04:10:29 +0530

    update function impl and cleanup

--- filters/src/gh.rs
+++ filters/src/gh.rs
@@ -1,6 +1,6 @@
-use ndarray::{ArrayD, ScalarOperand};
-use std::ops::AddAssign;
-use num::traits::{Float, Zero};
+use ndarray::{Array1, ScalarOperand};
+use num::traits::Float;
+use std::ops::{AddAssign, Mul};
 
 #[derive(Default)]
 struct State<T: Float + ScalarOperand> {
@@ -10,88 +10,26 @@ struct State<T: Float + ScalarOperand> {
 }
 
 #[derive(Default)]
-struct GhFilterConfig<T: Float + ScalarOperand> {
-    order: usize,
+struct GhkFilterConfig<T: Float + ScalarOperand> {
     g: T,
     h: T,
     k: T,
 }
 
-impl<T: Float + ScalarOperand> GhFilterConfig<T> {
-    fn checks(&self) -> Result<usize, &'static str> {
-        match self.order {
-            0 => {
-                if Zero::is_zero(&self.h) && Zero::is_zero(&self.k) {
-                    Ok(self.order)
-                } else {
-                    Err("h and k non-zero for order 0")
-                }
-            }
-            1 => {
-                if Zero::is_zero(&self.k) {
-                    Ok(self.order)
-                } else {
-                    Err("k non-zero for order 1")
-                }
-            }
-            2 => Ok(self.order),
-            _ => Err("Order should be in between 0 and 2"),
-        }
-    }
-}
-
-fn update<T>(state: &mut State<T>, config: &GhFilterConfig<T>, dt: &T) -> ()
+fn update<T>(state: &mut State<T>, config: &GhkFilterConfig<T>, z: &Array1<T>, dt: &T) -> ()
 where
-    T: Float + ScalarOperand + AddAssign,
+    T: Float + ScalarOperand + AddAssign + Mul<f64, Output = T>,
 {
-    match GhFilterConfig::<T>::checks(config).unwrap() {
-        0 => {
-            state.y = &state.z - &state.x;
-            state.x += &(&state.y * config.g);
-        }
+    let dt_sq = (*dt) * (*dt);
 
-//     match GhFilterOrder::<T>::check_valid_order(&self.order).unwrap() {
-//         0 => {
-//             self.y = z - self.x;
-//             self.x += &self.y * g_param;
-//         }
-//         1 => {
-//             let x = self.x.column(0);
-//             let dx = self.x.column(1);
-//             let dxdt = dx * self.dt;
+    // prediction step
+    let ddx_prediction = state.ddx.clone();
+    let dx_prediction = &state.dx + &(&state.ddx * (*dt));
+    let x_prediction = &state.x + &(&state.dx * (*dt)) + &((&state.ddx * dt_sq) * 0.5);
 
-//             self.y = z.clone() - (&x + &dxdt);
-
-//             let new_x = x.clone() + &dxdt + &self.y * g_param;
-//             // // FIXME
-//             // self.x.set_column(0, &new_x);
-
-//             let new_dx = dx + &self.y * h_param / self.dt;
-//             self.x.set_column(1, &new_dx);
-
-//             self.z = z;
-//         }
-//         2 => {
-//             let x = self.x.column(0);
-        _ => panic!("Not implemented for order not in {0, 1, 2}"),
-    }
+    // update step
+    let y = z - &x_prediction;
+    state.x = &x_prediction + &(&y * config.g);
+    state.dx = &dx_prediction + &(&y * config.h / (*dt));
+    state.ddx = &ddx_prediction + &(&y * config.k / dt_sq);
 }
-//             let T2 = self.dt.powi(2);
-
-//             // // FIXME
-//             // self.y = z - x + dxdt + (ddx * T2) * 0.5;
-
-//             // // FIXME
-//             // let new_x = x + dxdt + ddx.mul_to(T2, ddx) + 0.5 * ddx * T2 + self.y * g_param;
-//             // self.x.set_column(0, &new_x);
-
-//             // let new_dx = dx + ddx * self.dt + self.y * h_param / self.dt;
-//             // self.x.set_column(1, &new_dx);
-
-//             // // FIXME
-//             // let new_ddx = ddx + 2 * k_param * self.y / self.dt.powi(2);
-//             // self.x.set_column(2, &new_ddx);
-//         }
-//         _ => panic!("Not implemented for order not in {0, 1, 2}"),
-//     }
-// }
