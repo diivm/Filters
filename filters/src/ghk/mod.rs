884412674cc990ba242241b0a5ec846cb926eac2
Divyanshu Madan <iamdm99@gmail.com>
2020-11-18 03:05:38 +0530

    add more tests (tdd; not passing yet)

--- filters/src/ghk/mod.rs
+++ filters/src/ghk/mod.rs
@@ -10,13 +10,13 @@ struct State<T: Float + ScalarOperand> {
 }
 
 #[derive(Default, Debug)]
-struct GhkFilterConfig<T: Float + ScalarOperand> {
+struct GHKFilterConfig<T: Float + ScalarOperand> {
     g: T,
     h: T,
     k: T,
 }
 
-fn update<T>(state: &mut State<T>, config: &GhkFilterConfig<T>, z: &Array1<T>, dt: &T) -> ()
+fn update<T>(state: &mut State<T>, config: &GHKFilterConfig<T>, z: &Array1<T>, dt: &T) -> ()
 where
     T: Float + ScalarOperand + AddAssign + Mul<f32, Output = T>,
 {
@@ -41,11 +41,19 @@ where
 
 #[cfg(test)]
 mod UnitTests {
+    use gnuplot::{Caption, Color, Figure};
     use ndarray::{arr1, Array1};
+    use rand::random;
+    use std::env;
+
     #[test]
     fn test_update_function_random_values() {
-        // Values taken from:
-        // https://nbviewer.jupyter.org/github/rlabbe/Kalman-and-Bayesian-Filters-in-Python/blob/master/01-g-h-filter.ipynb
+        /*
+        **********
+        Values taken from:
+        https://nbviewer.jupyter.org/github/rlabbe/Kalman-and-Bayesian-Filters-in-Python/blob/master/01-g-h-filter.ipynb
+        **********
+        */
         let x = arr1::<f32>(&[1.0, 10.0, 100.0]);
         let x_dim = x.raw_dim();
         let dx = arr1::<f32>(&[10.0, 12.0, 0.2]);
@@ -55,7 +63,7 @@ mod UnitTests {
             dx: dx,
             ddx: ddx,
         };
-        let config = super::GhkFilterConfig::<f32> {
+        let config = super::GHKFilterConfig::<f32> {
             g: 0.8,
             h: 0.2,
             ..Default::default()
@@ -69,5 +77,66 @@ mod UnitTests {
 
         assert_eq!(state.x, arr1::<f32>(&[3.7999997, 13.2, 101.64]));
         assert_eq!(state.dx, arr1::<f32>(&[8.2, 9.8, 0.5600006]));
+    }
+
+    /*
+    **********
+    Tests from https://github.com/rlabbe/filterpy/blob/master/filterpy/gh/tests/test_gh.py
+    **********
+    */
+    #[test]
+    fn test_least_squares() {
+        /*
+        There is an alternative form for computing h for the least squares.
+        It works for all but the very first term (n=0). Use it to partially test
+        the output of least_squares_parameters(). This test does not test that
+        g is correct.
+        */
+        for n in 1..100 {
+            let config = super::least_squares_parameters(n);
+            let h2 = 4.0
+                - 2.0 * config.g
+                - (4.0 * (config.g - 2.0).powf(2.0) - 3.0 * config.g.powf(2.0)).powf(0.5);
+            assert!((h2 - config.h).abs() < 1e-12);
+        }
+    }
+
+    #[test]
+    #[cfg(GRAPH_TESTS)]
+    fn test_optimal_smoothing() {
+        fn fx(x: f32) -> Array1<f32> {
+            arr1::<f32>(&[0.1 * x.powf(2.0) + 3.0 * x - 4.0])
+        }
+
+        let config = super::optimal_noise_smoothing(0.2);
+
+        let x = arr1::<f32>(&[4.0]);
+        let x_dim = x.raw_dim();
+        let dx = Array1::<f32>::zeros(x_dim);
+        let ddx = Array1::<f32>::zeros(x_dim);
+        let mut state = super::State::<f32> {
+            x: x,
+            dx: dx,
+            ddx: ddx,
+        };
+        let dt = 1.0;
+
+        let mut ys = Vec::new();
+        let mut zs = Vec::new();
+
+        for i in 0..100 {
+            let z = fx(i as f32) + rand::random::<f32>() * 10.0;
+            super::update::<f32>(&mut state, &config, &z, &dt);
+            ys.push(state.x[0]);
+            zs.push(z[0]);
+        }
+
+        let time: Vec<u32> = (0..100).collect();
+        let mut fg = Figure::new();
+        fg.axes2d()
+            .lines(&ys, &time, &[Caption("State x"), Color("blue")]);
+        fg.axes2d()
+            .lines(&zs, &time, &[Caption("Observation"), Color("red")]);
+        fg.show();
     }
 }
